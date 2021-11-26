1a6314dd4ff3403458a38e6fe982eb1ec50b7a1d
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2021-11-26 23:39:33 +0100

    Lesson 216 done

---
+++ 14-OOP-with-JavaScript/216-Object.Create/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const PersonProto = {
+	calcAge() {
+		console.log(2037 - this.birthYear);
+	},
+
+	init(firstName, birthYear) {
+		this.firstName = firstName;
+		this.birthYear = birthYear;
+	},
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+
+steven.name = "Steven";
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__);
+console.log(PersonProto);
+console.log(steven.__proto__ === PersonProto);
+
+const sara = Object.create(PersonProto);
+
+sara.init("Sarah", 1979);
+sara.calcAge();
