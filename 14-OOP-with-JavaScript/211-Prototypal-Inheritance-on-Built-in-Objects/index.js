c513ccddfcefa06a647830b5f8d6a0dc73858c7b
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2021-11-26 21:13:39 +0100

    Challenge-1 done

--- 14-OOP-with-JavaScript/211-Prototypal-Inheritance-on-Built-in-Objects/index.js
+++ 14-OOP-with-JavaScript/211-Prototypal-Inheritance-on-Built-in-Objects/index.js
@@ -75,3 +75,54 @@ Array.prototype.unique = function () {
 
 console.log(arr.unique());
 console.dir((x) => x + 1);
+
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car 
+has a make and a speed property. The speed property is 
+the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase 
+the car's speed by 10, and log the new speed to the 
+console;
+3. Implement a 'brake' method that will decrease the 
+car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 
+'accelerate' and 'brake' multiple times on each of them.
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+GOOD LUCK 😀
+
+*/
+
+//1.
+
+const Car = function (speed, make) {
+	this.speed = speed;
+	this.make = make;
+};
+
+//2.
+
+Car.prototype.accelerate = function () {
+	console.log((this.speed += 10));
+};
+
+//3.
+Car.prototype.brake = function () {
+	console.log((this.speed -= 5));
+};
+
+//4.
+
+const car1 = new Car(120, "BMW");
+const car2 = new Car(95, "Mercedes");
+
+console.log(car1, car2);
+
+car1.accelerate();
+car1.accelerate();
+car2.accelerate();
+
+car1.brake();
+car2.brake();
