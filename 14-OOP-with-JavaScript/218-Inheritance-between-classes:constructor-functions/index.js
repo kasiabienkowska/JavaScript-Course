a2ce6864cc787a91192fe29cf9e0d6c6befeafc3
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2021-11-27 23:13:13 +0100

    Lesson 218 done

---
+++ 14-OOP-with-JavaScript/218-Inheritance-between-classes:constructor-functions/index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const Person = function (firstName, birthYear) {
+	this.firstName = firstName;
+	this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+	console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+	// this.firstName = firstName;
+	// this.birthYear = birthYear;
+	Person.call(this, firstName, birthYear);
+	this.course = course;
+};
+
+//Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+	console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student("Mike", 2020, "Computer Science");
+
+console.log(mike);
+
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+Student.prototype.constructor = Student;
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+
+console.dir(Student.prototype.constructor);
