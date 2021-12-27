f31b32b01f0b4bf8232caa16323c6d8a9eb8478e
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2021-12-27 21:26:37 +0100

    Lesson 116 done

---
+++ 09-Data-Structures-Operators/116-Sets/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const ordersSet = new Set([
+	"Pasta",
+	"Pizza",
+	"Pizza",
+	"Risotto",
+	"Pasta",
+	"Pizza",
+]);
+
+console.log(ordersSet);
+
+console.log(new Set("Jonas"));
+
+console.log(ordersSet.size);
+
+console.log(ordersSet.has("Pizza"));
+
+console.log(ordersSet.has("Bread"));
+
+ordersSet.add("Garlic Bread");
+ordersSet.add("Garlic Bread");
+ordersSet.delete("Risotto");
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+//Example
+
+const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+
+// const staffUnique = new Set(staff);
+const staffUnique = [...new Set(staff)];
+
+console.log(staffUnique);
+console.log(
+	new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
+);
+console.log(new Set(staff).size);
+
+console.log(new Set("chrząszcz brzmi w trzcinie w szczebrzeszynie").size);
+console.log(new Set("kasia").size);
